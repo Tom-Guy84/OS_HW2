7fc989e02b3d0516e1b984e73cd6c4a1b7ad7384
Tom-Guy84 <83006932+Tom-Guy84@users.noreply.github.com>
2022-05-22 20:15:13 +0300

    Add files via upload

--- kernel/hw2.c
+++ kernel/hw2.c
@@ -1,1 +1,28 @@
+#include <linux/kernel.h>
+#include <linux/list.h>
+#include <linux/module.h>
+#include <linux/sched.h>
 
+asmlinkage long sys_hello(void) {
+	 printk("Hello, World!\n");
+	 return 0;
+}
+
+asmlinkage int sys_set_status(int status){
+	if(status < 0 || status > 1)
+		return -EINVAL;
+       	current->fac = status;
+ 	return 0;
+}
+
+asmlinkage int sys_get_status(void){
+	return current->fac;
+}
+
+asmlinkage int sys_register_process(void){
+	return 0;	
+}
+
+asmlinkage long sys_get_all_cs(void){
+	return 0;
+}
